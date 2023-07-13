13b538846aadd1d4426e981ec176f6b0e559bfbe
Shibata-Nanami <n.shibata@visionary-jp.net>
2023-07-13 17:32:48 +0900

    削除ボタンの作成

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,11 +1,10 @@
 /// メモ一覧画面用のWidget
 import 'package:flutter/material.dart';
 import 'package:flutter_task_3/todo_add_page.dart';
-
-import 'models/model.dart';
+import 'package:flutter_task_3/Model.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required String titleName});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -38,29 +37,47 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: dummyDataList.length,
-                itemBuilder: (context, index) {
-                  //以下の記載でModelクラスの単体のデータ取得が可能
-                  final Model modelData = dummyDataList[index];
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 20),
-                    child: Container(
-                      alignment: Alignment.centerLeft,
-                      width: 80,
-                      height: 80,
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 30.0),
-                        //以下の記載でModelクラスのタイトル
-                        child: Text(modelData.title),
-                      ),
+              shrinkWrap: true,
+              itemCount: dummyDataList.length,
+              itemBuilder: (context, index) {
+                //以下の記載でModelクラスの単体のデータ取得が可能
+                final Model modelData = dummyDataList[index];
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 20),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    width: 80,
+                    height: 80,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                  );
-                }),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30), // 左側に空白を追加
+                          child: Text(modelData.title),
+                        ),
+                        const Spacer(), //右側に寄せる
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20), //右側に空白を追加
+                          child: InkWell(
+                            child: IconButton(
+                              icon: const Icon(Icons.delete),
+                              iconSize: 35, // アイコンのサイズを指定
+                              onPressed: () {
+                                // ボタンがタップされた時の処理
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ),
